ce3cb0a2fabbc59271230c97b77303dbde930f47
Sharmaine Teo <sharmaineteo@Shenyis-Mac-mini.local>
2023-02-08 23:55:38 +0800

    Add SearchBar component

--- src/components/Dashboard/SearchBar.js
+++ src/components/Dashboard/SearchBar.js
@@ -1,19 +1,44 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text, TextInput } from "react-native";
 import colours from "../../styles/colours";
-import InputField from "../UI/InputField";
 
 export default function SearchBar() {
   return (
-    <InputField placeholder={"Search"}>
-      <View style={styles.userLogo} />
-    </InputField>
+    <View style={styles.container}>
+      <View style={styles.searchBar__unclicked}>
+        <TextInput placeholder={"Search"} styles={styles.searchInput} />
+        <View style={styles.userLogo} />
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    shadowColor: "#171717",
+    shadowOffset: { width: -2, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    borderRadius: 15,
+    height: 50,
+    margin: 20,
+    padding: 10,
+    flexDirection: "row",
+    position: "absolute",
+    top: 50,
+  },
+  searchBar__unclicked: {
+    flexDirection: "row",
+    width: "95%",
+    borderRadius: 15,
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
   userLogo: {
     backgroundColor: colours.pink,
+    width: 30,
+    height: 30,
     borderRadius: 99,
   },
 });
--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,15 +1,19 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import { Card } from "../components/UI/Card";
 import SearchBar from "../components/Dashboard/SearchBar";
+import BottomModal from "../components/Dashboard/BottomModal";
 
 const Dashboard = () => {
   return (
     <Card>
       <SearchBar />
-      <Text> Dashboard </Text>
+      <BottomModal>
+        <Text>Most accessible places</Text>
+      </BottomModal>
     </Card>
   );
 };
 
+const styles = StyleSheet.create({});
 export default Dashboard;
