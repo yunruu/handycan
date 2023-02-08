4e7a6201aab1b3f781616c7179edc8850d634e3c
Sharmaine Teo <sharmaineteo@Shenyis-Mac-mini.local>
2023-02-08 23:55:38 +0800

    Add SearchBar component

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,13 +1,19 @@
-import { View, Text } from 'react-native';
-import React from 'react';
-import { styles } from '../style/Styles';
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { Card } from "../components/UI/Card";
+import SearchBar from "../components/Dashboard/SearchBar";
+import BottomModal from "../components/Dashboard/BottomModal";
 
-const Dashboard = ({navigation}) => {
-    return (
-        <View style={styles.containerWhite}>
-            <Text> Dashboard </Text>
-        </View>
-    )
+const Dashboard = () => {
+  return (
+    <Card>
+      <SearchBar />
+      <BottomModal>
+        <Text>Most accessible places</Text>
+      </BottomModal>
+    </Card>
+  );
 };
 
+const styles = StyleSheet.create({});
 export default Dashboard;
