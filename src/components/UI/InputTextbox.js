3666aec5b2d8f576f9c99e9c6cd0331345625ebb
yunruu <yunruuchin@gmail.com>
2023-02-09 18:01:08 +0800

    Add input textbox component

---
+++ src/components/UI/InputTextbox.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { TextInput, StyleSheet, View } from "react-native";
+import { styles, text } from "../../style/Styles";
+
+export default function InputTextbox(props, { placeholder, onChangeText, secureTextEntry }) {
+  return (
+    <View style={styles.textContainer1}>
+      <TextInput
+        placeholder={props.placeholder}
+        onChangeText={props.onChangeText}
+        style={text.caption16}
+        secureTextEntry={props.secureTextEntry}
+      />
+      {props.children}
+    </View>
+  );
+}
