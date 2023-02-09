04cd1df04f591d3c16cf428d5dbb89d92b7156f1
Adee Aryaa <e0726108@u.nus.edu>
2023-02-09 13:48:21 +0800

    Add Authentication

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,12 +1,52 @@
 import { View, Text } from 'react-native';
+import React, { Component, useState } from "react";
 import React from 'react';
 
-const Login = () => {
-    return (
-        <View>
-            <Text> Login </Text>
-        </View>
-    )
-};
+const __isValidEmail = email => {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  };
 
-export default Login
+const LoginComponent = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [fetching, setFetching] = useState(false);
+    const [error, setError] = useState("");
+    const [isValid, setValid] = useState(true);
+    const __doLogin = () => {
+        if (!email) {
+            setError("Email required *");
+            setValid(false);
+            return;
+        } else if (!password && password.trim() && password.length > 6) {
+            setError("Weak password, minimum 5 chars");
+            setValid(false);
+            return;
+        } else if (!__isValidEmail(email)) {
+            setError("Invalid Email");
+            setValid(false);
+            return;
+        }
+        let signInRequestData = {
+            email,
+            password
+        };
+
+        __doSignIn(email, password);
+    };
+
+    const __doSignIn = async (email, password) => {
+        try {
+            let response = await auth().signInWithEmailAndPassword(email, password);
+            if (response && response.user) {
+                Alert.alert("Success ✅", "Logged successfully");
+            }
+        } catch (e) {
+            console.error(e.message);
+        }
+    };
+
+    return (
+        
+    );
+}
--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,12 +1,50 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 
-const Register = () => {
+const __isValidEmail = email => {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  };
+
+
+const SignUpComponent = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [fetching, setFetching] = useState(false);
+    const [error, setError] = useState("");
+    const [isValid, setValid] = useState(true);
+    const __doSignUp = () => {
+      if (!email) {
+        setError("Email required *");
+        setValid(false);
+        return;
+      } else if (!password && password.trim() && password.length > 6) {
+        setError("Weak password, minimum 5 chars");
+        setValid(false);
+        return;
+      } else if (!__isValidEmail(email)) {
+        setError("Invalid Email");
+        setValid(false);
+        return;
+      }
+  
+      __doCreateUser(email, password);
+    };
+  
+    const __doCreateUser = async (email, password) => {
+      try {
+        let response = await auth().createUserWithEmailAndPassword(email, password);
+        if (response && response.user) {
+          Alert.alert("Success ✅", "Account created successfully");
+        }
+      } catch (e) {
+        console.error(e.message);
+      }
+    };
+  
     return (
-        <View>
-            <Text> Register </Text>
-        </View>
-    )
-};
+      
+    );
+  };
 
 export default Register
