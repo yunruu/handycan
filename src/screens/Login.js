1bdd2b9a172653cc51a54ca687289df47ebf458a
yunruu <yunruuchin@gmail.com>
2023-02-09 15:29:31 +0800

    Implement login screen

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
-import { View, Text, StyleSheet, TouchableOpacity, Button, Image } from 'react-native';
-import { styles } from '../style/Styles';
+import { View, Text, StyleSheet, TouchableOpacity, Pressable, Image, Alert } from 'react-native';
+import { buttons, logos, styles, text } from '../style/Styles';
+import { COLORS } from '../style/Colors';
 
 // const auth = getAuth();
 
 function Login() {
     return (
-        <View style={styles.text_container_1}>
-            <Text>Login</Text>
+        <View style={styles.containerWhite}>
+            <Image 
+                style={logos.mediumLogo}
+                source={require("../../assets/handycan-logo.png")}></Image>
+            <View style={styles.textContainer1}>
+                <Text style={text.placeholder16}>
+                    Email
+                </Text>
+            </View>
+            <View style={styles.textContainer1}>
+                <Text style={text.placeholder16}>
+                    Password
+                </Text>
+            </View>
+                <Pressable style={buttons.primaryPink}
+                    onPress={() => Alert.alert('Login pressed')}>
+                        <Text style={text.colorButtonText}>Login</Text>
+                </Pressable>
         </View>
     )
 }
--- src/style/Styles.js
+++ src/style/Styles.js
@@ -2,30 +2,96 @@ import { StyleSheet } from "react-native";
 import { COLORS } from "./Colors";
 
 const styles = StyleSheet.create({
-    container: {
-        flexWrap: 'wrap',
-        flexDirection: 'column'
+    containerWhite: {
+        flex: 1,
+        backgroundColor: COLORS.white,
+        alignItems: "center",
+        justifyContent: 'center',
     },
 
-    text_container_1: {
-        flexWrap: 'wrap',
-        alignItems: 'flex-start',
+    containerPink: {
+        flex: 1,
+        backgroundColor: COLORS.pink
+    },
+
+    containerBlue: {
+        backgroundColor: COLORS.pink
+    },
+
+    textContainer1: {
         backgroundColor: '#FFFFFF',
+        borderRadius: 30,
         shadowColor: '#171717',
         shadowOffset: {width: 2, height: 4},
         shadowOpacity: 0.2,
         shadowRadius: 3,
-        height: 150,
-        width: 150,
+        height: 50,
+        width: 300,
+        margin: 16,
+        paddingLeft: 21,
+        paddingTop: 15,
     }
 })
 
 const buttons = StyleSheet.create({
-    primary: {
-        flex: 1,
+    primaryPink: {
         height: 50,
-        backgroundColor: COLORS.white
+        backgroundColor: COLORS.pink,
+        height: 45,
+        width: 150,
+        borderRadius: 30,
+        margin: 15,
+        alignItems: 'center',
+        justifyContent: 'center',
     },
 })
 
-export { styles, buttons }
+const text = StyleSheet.create({
+    header: {
+        // fontFamily: 'Inter',
+        fontSize: 32,
+    },
+    body: {
+        // fontFamily: 'Inter',
+        fontSize: 20,
+    },
+
+    caption: {
+        // fontFamily: 'Inter',
+        fontSize: 14,
+    },
+
+    placeholder16: {
+        // fontFamily: 'Inter',
+        fontSize: 16,
+        color: COLORS.light_gray_02,
+    },
+
+    placeholder14: {
+        // fontFamily: 'Inter',
+        fontSize: 14,
+        color: COLORS.light_gray_02,
+    },
+
+    colorButtonText: {
+        // fontFamily: 'Inter',
+        fontSize: 16,
+        fontWeight: "bold",
+        color: COLORS.white,
+    },
+})
+
+const logos = StyleSheet.create({
+    tinyLogo: {
+        width: 50,
+        height: 50,
+    },
+
+    mediumLogo: {
+        width: 120,
+        height: 120,
+        margin: 15,
+    },
+});
+
+export { styles, buttons, text, logos }
