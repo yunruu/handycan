eadcb2a9b8e5627119aaae9ca542c4a52590da85
yunruu <yunruuchin@gmail.com>
2023-02-09 14:34:46 +0800

    Fix firebase config

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getAuth} from "firebase/app";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -20,6 +21,7 @@ const firebaseConfig = {
 // Initialize Firebase and Firestore
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 const analytics = getAnalytics(app);
 
-export { db };
+export { db, auth };
