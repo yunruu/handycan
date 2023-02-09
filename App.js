986fe703271aaec4bddbe56adf0bf631628c61d9
yunruu <yunruuchin@gmail.com>
2023-02-09 14:07:31 +0800

    Abstract login and register page out of App.js

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StyleSheet, Text, View } from 'react-native';
+import RootNavigation from './src/navigation/Index';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <RootNavigation/>
+  )
 }
 
 const styles = StyleSheet.create({
