460564fab9063ecde369bd6162bf329b6901ac68
yunruu <yunruuchin@gmail.com>
2023-02-10 03:18:15 +0800

    Fix file naming bug

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import AuthStack from "./src/navigation/authStack";
-import UserStack from "./src/navigation/userStack";
+import AuthStack from "./src/navigation/AuthStack";
+import UserStack from "./src/navigation/UserStack";
 import Dashboard from "./src/screens/Dashboard";
 import LoginScreen from "./src/screens/LoginScreen";
 import SearchPage from "./src/screens/SearchPage";
