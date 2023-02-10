6e555a61a96c03c8c6f55a8946ed610a531bc5f8
yunruu <yunruuchin@gmail.com>
2023-02-10 22:41:36 +0800

    Update feedback page

--- src/screens/Feedback.js
+++ src/screens/Feedback.js
@@ -1,12 +1,32 @@
-import { View, Text, Pressable, Image } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, Alert, TouchableOpacity, Image } from 'react-native'
+import React, { useState } from 'react'
 import ReturnButton from '../components/UI/ReturnButton'
-import { BUTTONS, STYLES, TEXTS } from '../style/Styles'
+import PinkButton from '../components/UI/PinkButton'
+import { BUTTONS, STYLES, TEXTS, LOGOS } from '../style/Styles'
 import WhiteBottomSheet from '../components/UI/WhiteBottomSheet'
 import PageHeader from '../components/UI/PageHeader'
-import RatingBar from '../components/UI/RatingBar'
+import DropdownMenu from '../components/UI/DropdownMenu'
 
-function Feedback(props, { navigation, restaurant }) {
+const disabilityType = ["Audio", "Mobility", "Visual", "Mental", "Other"];
+const maxRating = [1,2,3,4,5];
+
+function Feedback({ navigation, restaurant }) {
+    const [rating, setRating] = useState(0);
+    const [feedback, onChangeFeedback] = useState("");
+    const [disability, setDisability] = useState("");
+
+    const submitFeedback = () => {
+        if (feedback == "") {
+            Alert.alert("There is no feedback. \n Write one now!")
+        }
+        if (disability == "") {
+            Alert.alert("Select a disability type.");
+        }
+        if (rating == 0) {
+            Alert.alert("Give an accessibility rating.")
+        }
+        // pass feedback and disability to firebase on submit
+    };
 
     return (
         <View style={STYLES.containerPink}>
@@ -17,11 +37,40 @@ function Feedback(props, { navigation, restaurant }) {
                 <View style={{ alignItems: 'center' }}>
                     <Text style={TEXTS.subHeaderBlack}>Random Burger Place</Text>
                     <View style={STYLES.transparentContainerHor}>
-                        <Text style={[TEXTS.caption, {paddingRight: 5}]}>
+                        <Text style={[TEXTS.caption16, {paddingRight: 5}]}>
                             Accessibility Rating:
                         </Text>
-                        <RatingBar />
+                        
+                        <View style={STYLES.ratingBar}>
+                            {
+                                maxRating.map((item, key) => {
+                                    return(
+                                        <TouchableOpacity 
+                                            activeOpacity={0.7} 
+                                            key={item} 
+                                            onPress={() => { setRating(item); }}>
+                                                <Image source={item <= rating
+                                                            ? require("../../assets/star-filled.png")
+                                                            : require("../../assets/star-empty.png")} 
+                                                            style={[LOGOS.microLogo, {marginLeft: 2}]} />
+                                        </TouchableOpacity>
+                                    )
+                                })
+                            }
+                        </View>
+
                     </View>
+                    <DropdownMenu 
+                        options={disabilityType} 
+                        onSelect={(disability) => setDisability(disability)}/>
+                    <View style={STYLES.feedbackContainer}>
+                        <TextInput
+                            multiline={true}
+                            maxLength={400}
+                            onChangeText={feedback => onChangeFeedback(feedback)}
+                            placeholder={"Leave a feedback"} />
+                    </View>
+                    <PinkButton textField={"Submit"} onPress={() => submitFeedback()}/>
                 </View>
             </WhiteBottomSheet>
         </View>
