ba36a3da686a4d6af56257c91759df06aac7dde0
yunruu <yunruuchin@gmail.com>
2023-02-10 14:47:28 +0800

    Add profile icon to dashboard and search page

--- src/screens/SearchPage.js
+++ src/screens/SearchPage.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Text, Image, Pressable, View } from "react-native";
+import { Text, Image, Pressable, View, StyleSheet } from "react-native";
 import BottomSheet from "../components/Dashboard/BottomSheet";
 import SearchBar from "../components/Dashboard/SearchBar";
 import { Card } from "../components/UI/Card";
+import ProfileIcon from "../components/UI/ProfileIcon";
 import { LOGOS } from "../style/Styles";
 
 function SearchPage ({ locationName, navigation }) {
@@ -10,14 +11,16 @@ function SearchPage ({ locationName, navigation }) {
   const onChangeLocation = () => {};
   return (
     <Card>
-      <View style={{flexDirection: 'row'}}>
+      {/* <View style={styles.container}>
         <Pressable onPress={() => navigation.navigate("Login")}>
           <Image style={LOGOS.microLogo}
                 source={require("../../assets/profile_icon.png")} />
         </Pressable>
-      </View>
+      </View> */}
+      <ProfileIcon onPress={() => navigation.navigate("Login")} 
+        style={styles.container} />
       
-      <SearchBar value={locationName} sonChangeText={onChangeLocation} />
+      <SearchBar value={locationName} onChangeText={onChangeLocation} />
       <BottomSheet>
         <Text>{locationName}</Text>
       </BottomSheet>
@@ -26,3 +29,12 @@ function SearchPage ({ locationName, navigation }) {
 };
 
 export default SearchPage;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    position: "absolute",
+    top: 50,
+    left: 15,
+  },
+});
--- src/style/Styles.js
+++ src/style/Styles.js
@@ -118,4 +118,13 @@ const LOGOS = StyleSheet.create({
     },
 });
 
-export { STYLES, BUTTONS, TEXTS, LOGOS };
+const SHADOWS = StyleSheet.create({
+    bottomRight: {
+        shadowColor: "#171717",
+        shadowOffset: { width: -2, height: 4 },
+        shadowOpacity: 0.2,
+        shadowRadius: 3,
+    }
+})
+
+export { STYLES, BUTTONS, TEXTS, LOGOS, SHADOWS };
