5f9e0b29d1724dc71527359452f893ec45930114
glemenneo <glemenneo@gmail.com>
2023-02-10 20:27:25 +0800

    Add enums for tagging, filtering and sorting

---
+++ src/utils/DisabilityTag.js
@@ -0,0 +1,10 @@
+//disability tag enum
+const disabilityTag = {
+    AUDIO: "Audio Accessibility",
+    MOBILITY: "Mobility Accessibility",
+    VISUAL: "Visual Accessibility",
+    MENTAL: "Mental Accessibility",
+    OTHER: "Other Accessibilities",
+}
+
+export { disabilityTag };
