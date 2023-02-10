315d8ef2e5ee75949ccbfbda20ad5b583225b6cf
glemenneo <glemenneo@gmail.com>
2023-02-11 02:33:27 +0800

    Hide firebase api keys by dotenv

--- babel.config.js
+++ babel.config.js
@@ -5,15 +5,8 @@ module.exports = function (api) {
       "babel-preset-expo"
     ],
     plugins: [
-      "react-native-reanimated/plugin", 
-      ["module:react-native-dotenv", {
-        "moduleName": "@env",
-        "path": ".env",
-        "blacklist": null,
-        "whitelist": null,
-        "safe": false,
-        "allowUndefined": true
-      }]
+      "react-native-reanimated/plugin",
+      "inline-dotenv",
     ]
   };
 };
--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -4,26 +4,17 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import {
-  REACT_APP_API_KEY, 
-  REACT_APP_AUTH_DOMAIN, 
-  REACT_APP_PROJECT_ID,
-  REACT_APP_STORAGE_BUCKET,
-  REACT_APP_MESSAGING_SENDER_ID,
-  REACT_APP_APP_ID,
-  REACT_APP_MEASUREMENT_ID
-} from '@env';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: REACT_APP_API_KEY,
-  authDomain: REACT_APP_AUTH_DOMAIN,
-  projectId: REACT_APP_PROJECT_ID,
-  storageBucket: REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
-  appId: REACT_APP_APP_ID,
-  measurementId: REACT_APP_MEASUREMENT_ID
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase and Firestore
